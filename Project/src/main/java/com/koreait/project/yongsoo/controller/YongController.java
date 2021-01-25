b12704d78071f73cb1610d8d8d62b38acf47bbeb
plitche <kysu728@naver.com>
2021-01-25 21:34:29 +0900

    초기 세팅작업 추가 실시
    pom.xml 파일에 히카리, jackson 디펜전시 추가
    root-context.xml 파일에 경로 변경 및 히카리 적용 태그 추가
    공통으로 사용 될 index.jsp 용 Controller 및 Map을 반환하는 Command 반환 타입이 없는 VoidCommand 파일 작업

--- Project/src/main/java/com/koreait/project/yongsoo/controller/YongController.java
+++ Project/src/main/java/com/koreait/project/yongsoo/controller/YongController.java
@@ -1,5 +1,14 @@
 package com.koreait.project.yongsoo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class YongController {
 
+	@RequestMapping(value="/")
+	public String goindex() {
+		return "index";
+	}
+	
 }
