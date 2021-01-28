3214449037d3ce8f48d6214c5ca732d386ececc9
plitche <kysu728@naver.com>
2021-01-29 00:51:21 +0900

    Main 페이지에서 트레이너 List페이지 이동 작업
    Main 페이지에서 트레이너 List페이지로 이동 작업하기 위한 작업 실시
    개인 패키지 파일들 (dto, dao, mapper, controller, command) 파일 제외하고,
    공통파일 header 파일 트레이너 <a>태그에 경로 추가함.
    원하는 정보만 가져오기 위한 임시 trainerTemDto 추가 생성

--- Project/src/main/java/com/koreait/project/yongsoo/dao/TrainerDao.java
+++ Project/src/main/java/com/koreait/project/yongsoo/dao/TrainerDao.java
@@ -1,5 +1,12 @@
 package com.koreait.project.yongsoo.dao;
 
-public class SooDao {
+import java.util.List;
 
+import com.koreait.project.yongsoo.dto.TrainerTemDto;
+
+public interface TrainerDao {
+
+	public List<TrainerTemDto> trainerList();
+	
+	public TrainerTemDto trainerDetail(int user_no);
 }
---
+++ Project/src/main/java/com/koreait/project/yongsoo/dto/TrainerTemDto.java
@@ -0,0 +1,23 @@
+package com.koreait.project.yongsoo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TrainerTemDto {
+
+	private int user_no;
+	private String user_nickname;
+	private String trainer_name;
+	private String user_message;
+	private String profile_photo;
+	private int career;	// 1년,2년,3년 등 #연차로 표기
+	private String employment;
+	private String profile;
+	private int location1_no;
+	private int location2_no;
+	
+}
