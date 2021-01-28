93eaed078c3163a52d9d9f684976d43e6baff0e2
DESKTOP-L7VVF0L\ITSC <kysu728@naver.com>
2021-01-28 15:48:14 +0900

    공통 Dto 작업 실시
    CommentDto
    ExerciseDto
    Is_reviewedDto
    Location1Dto
    Location2Dto
    MaterialsDto
    Meeting_participantsDto
    MeetingDto
    PhotoDto
    User_interestDto

---
+++ Project/src/main/java/com/koreait/project/Dto/ExerciseDto.java
@@ -0,0 +1,15 @@
+package com.koreait.project.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ExerciseDto {
+
+	private int exercise_no;
+	private String exercise_name;
+	
+}
