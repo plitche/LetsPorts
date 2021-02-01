357aa2a9ded871e077739443ebc1e1243a76fe58
YewonLimm <limyeng@naver.com>
2021-02-01 22:17:20 +0900

    insertCommand 작업 수정
    노하우 게시판 insertCommand 작업

--- Project/src/main/java/com/koreait/project/yewon/command/GoknowHowInsertCommand.java
+++ Project/src/main/java/com/koreait/project/yewon/command/GoknowHowInsertCommand.java
@@ -1,9 +1,12 @@
 package com.koreait.project.yewon.command;
 
+import java.util.HashMap;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.jdbc.core.metadata.SqlServerCallMetaDataProvider;
 import org.springframework.ui.Model;
 
+import com.koreait.project.dto.Board_knowhowDto;
 import com.koreait.project.yewon.dao.KnowHowDao;
 import com.sun.javafx.collections.MappingChange.Map;
 
@@ -12,8 +15,12 @@ public class GoknowHowInsertCommand {
 	@Override
 	public Map<String, Object> execute(SqlSession sqlSession, Model model) {
 		
-		KnowHowDao knowHowDao = (KnowHowDao)map.get("Board_knowHowDto");
+		Board_knowhowDto Board_knowhowDto = (Board_knowhowDto)map.get("Board_knowhowDto");
+		KnowHowDao KnowHowDao = sqlSession.getMapper(KnowHowDao.class);
+		KnowHowDao knowHowDao = KnowHowDao.knowHowList();
 		
+		
+		Map<String, Object> result = new HashMap();
 		
 		
 		
