f1f09baf54acef92f42fd63e99e2f392e76d4ccc
plitche <kysu728@naver.com>
2021-02-02 01:42:20 +0900

    2021.02.01 작업 commit (권용수)
    트레이너 List페이지에서 트레이너 View페이지로 이동 시 자동으로 QnA를 불러오는 작업 실시
    QnA 관련 처리만을 위한 controller, command, mapper, dao 파일 생성
    질문 작성 버튼 클릭시 modal창이 열리고 내용 작성 후 작성 완료를 누르게 되면, insert하는 작업까지 실시 (다시 점검 필요성 있음)

---
+++ Project/src/main/java/com/koreait/project/yongsoo/command/trainerQnA/GetTrainerQnACommand.java
@@ -0,0 +1,36 @@
+package com.koreait.project.yongsoo.command.trainerQnA;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+import com.koreait.project.common.CommonMapCommand;
+import com.koreait.project.dto.Trainer_qnaDto;
+import com.koreait.project.yongsoo.dao.TrainerQnADao;
+
+public class GetTrainerQnACommand implements CommonMapCommand {
+
+	@Override
+	public Map<String, Object> execute(SqlSession sqlSession, Model model) {
+
+		Map<String, Object> map = model.asMap();
+		int user_no = (int)map.get("user_no");
+		
+		TrainerQnADao trainerQnADao = sqlSession.getMapper(TrainerQnADao.class);
+		List<Trainer_qnaDto> qnalist = trainerQnADao.getTrainerQnAList(user_no); 
+		
+		Map<String, Object> result = new HashMap<String, Object>(); 
+		if (qnalist.size()>0) {
+			result.put("result", true);
+			result.put("qnaList", qnalist);
+		} else {
+			result.put("result", true);
+		}
+		
+		return result;
+	}
+
+}
