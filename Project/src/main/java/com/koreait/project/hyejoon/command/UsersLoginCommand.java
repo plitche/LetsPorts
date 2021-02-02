87057652f4012fd1291004ee468733f06f12da2e
HEYJUNIE <sangjinju0321@gmail.com>
2021-02-02 11:47:29 +0900

    로그인 수정 (2021.02.02)

--- Project/src/main/java/com/koreait/project/hyejoon/command/UsersLoginCommand.java
+++ Project/src/main/java/com/koreait/project/hyejoon/command/UsersLoginCommand.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.koreait.project.common.CommonVoidCommand;
 import com.koreait.project.hyejoon.dao.UsersDao;
@@ -19,18 +20,34 @@ public class UsersLoginCommand implements CommonVoidCommand {
 		Map<String, Object> map = model.asMap();
 		UsersDto usersDto = (UsersDto) map.get("usersDto");
 		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		RedirectAttributes redirect = (RedirectAttributes) map.get("redirect");
 		HttpSession session = request.getSession();
 		
 		UsersDao usersDao = sqlSession.getMapper(UsersDao.class);
 		UsersDto loginUser = usersDao.usersLogin(usersDto);
 		
+		// login정보를 체크한다.
+		if(loginUser == null) {
+			model.addAttribute("loginResult", false);
+			redirect.addFlashAttribute("loginResult");
+		} else {
+			model.addAttribute("loginResult", true);
+			
+			if(loginUser.getUser_separator() == 0) {
+				// 로그인 상태 유지 위해 session에 저장한다.
+				session.setAttribute("loginUser", loginUser);
+				redirect.addAttribute("loginUser", 0);
+				// 관리자 페이지로 넘겨준다.
+			} else if(loginUser.getUser_separator() == 1) {
+				session.setAttribute("loginUser", loginUser);
+				redirect.addAttribute("loginUser", 1);
+			} else {
+				session.setAttribute("loginUser", loginUser);
+				redirect.addAttribute("loginUser", 2);
+			}
+		}
 		
-		// user_id를 캐스팅하라 한 부분,,, userid라는 파라미터는 어디서 나온건지 몰라서 헤매는 중이여 용수님 ㅠㅠ
-		int user_no = Integer.parseInt(request.getParameter("user_no"));
-		String password = request.getParameter("password");
-		
-		session.setAttribute("user_no", usersDto);
-		session.setAttribute("password", usersDto);
 		
 	}
+	
 }
