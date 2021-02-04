e3a1755527da57060f50e31102e14f84b7e7507f
YewonLimm <limyeng@naver.com>
2021-02-04 11:37:47 +0900

    노하우 게시판 update페이지
    노하우 게시판 update페이지 넘어가는 작업

---
+++ Project/src/main/java/com/koreait/project/yewon/command/GoknowHowUpdateCommand.java
@@ -0,0 +1,16 @@
+package com.koreait.project.yewon.command;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+import com.koreait.project.common.CommonVoidCommand;
+
+public class GoknowHowUpdateCommand implements CommonVoidCommand {
+
+	@Override
+	public void execute(SqlSession sqlSession, Model model) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
