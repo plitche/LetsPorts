7a0dd7813815c00fc8f74296bd97f833ec8fd45d
plitche <kysu728@naver.com>
2021-02-13 04:14:50 +0900

    트레이너 모임 리뷰 파트 작업
    1. 트레이너 모임 리뷰 파트 css 작업 실시
    2. 기존 트레이너 모임 리뷰 작성시 평점을 숫자로 입력해줘야 했던 부분 별점으로 교체함(현재는 1점 단위로 만 가능함)
    3. 리뷰 파트 작성날짜 simpleDateFormat 처리함
    4. 더보기 / 접기로 추가적인 리뷰 내용을 보았던 부분을 화살표 표시로 바꿈
    5. 더보기 / 접기 부분 command 오류로 오작동 하던 부분 오류 수정

--- Project/src/main/webapp/resources/db/sooTestCase.sql
+++ Project/src/main/webapp/resources/db/sooTestCase.sql
@@ -2,22 +2,32 @@
 ALTER TABLE REVIEW MODIFY SCORE NUMBER DEFAULT 0
 
 -- user 임시용 3개 (트레이너)
-INSERT INTO USERS values (10, 'user10@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 1, 1, 0, 'naver', '쫑코치', '잘부탁드려요', 'trainer1', 1992, 01, 01, 0, 0);
-INSERT INTO USERS values (11, 'user11@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 2, 2, 0, 'naver', '뽀쌤', '잘부탁드려요', 'trainer2', 2000, 12, 31, 1, 1);
-INSERT INTO USERS values (12, 'user12@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 3, 3, 0, 'naver', 'Eddy코치', '잘부탁드려요', 'trainer3', 2000, 12, 31, 2, 1);
-INSERT INTO USERS values (13, 'user13@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 2, 2, 0, 'naver', 'Nina', '잘부탁드려요', 'trainer4', 2000, 12, 31, 1, 1);
-INSERT INTO USERS values (14, 'user14@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '천형', '잘부탁드려요', 'trainer5', 2000, 12, 31, 2, 1);
-INSERT INTO USERS values (15, 'user15@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '다이너마이트', '잘부탁드려요', 'trainer6', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (10, 'user10@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 1, 1, 0, 'naver', '쫑코치', '잘부탁드려요', 'trainer1.png', 1992, 01, 01, 0, 0);
+INSERT INTO USERS values (11, 'user11@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 2, 2, 0, 'naver', '뽀쌤', '잘부탁드려요', 'trainer2.png', 2000, 12, 31, 1, 1);
+INSERT INTO USERS values (12, 'user12@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 3, 3, 0, 'naver', 'Eddy코치', '잘부탁드려요', 'trainer3.jpg', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (13, 'user13@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 2, 2, 0, 'naver', 'Nina', '잘부탁드려요', 'trainer4.png', 2000, 12, 31, 1, 1);
+INSERT INTO USERS values (14, 'user14@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '천형', '잘부탁드려요', 'trainer5.png', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (15, 'user15@xxx.xxx', '1111', 1, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '다이너마이트', '잘부탁드려요', 'trainer6.png', 2000, 12, 31, 2, 1);
 
+INSERT INTO USERS values (30, 'user30@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 2, 2, 0, 'naver', '일반인1', '잘부탁드려요', 'profile30', 2000, 12, 31, 1, 1);
+INSERT INTO USERS values (31, 'user31@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 2, 2, 0, 'naver', '일반인2', '잘부탁드려요', 'profile31', 2000, 12, 31, 1, 1);
 INSERT INTO USERS values (32, 'user32@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 2, 2, 0, 'naver', '일반인3', '잘부탁드려요', 'profile32', 2000, 12, 31, 1, 1);
 INSERT INTO USERS values (33, 'user33@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '일반인4', '잘부탁드려요', 'profile33', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (34, 'user34@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '일반인5', '잘부탁드려요', 'profile34', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (35, 'user35@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '일반인6', '잘부탁드려요', 'profile35', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (36, 'user36@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '일반인7', '잘부탁드려요', 'profile36', 2000, 12, 31, 2, 1);
+INSERT INTO USERS values (37, 'user37@xxx.xxx', '1111', 2, SYSDATE, SYSDATE, 3, 3, 0, 'naver', '일반인8', '잘부탁드려요', 'profile37', 2000, 12, 31, 2, 1);
+
+INSERT INTO USERS values (38, 'trainer1@letsports.com', 1111, 1, SYSDATE, SYSDATE, 1, 0, 0, 'naver', '쫑트', '잘부탁드려요', 'trainer1.png', null, null, null, 0, 0);
+INSERT INTO USERS values (39, 'trainer2@letsports.com', 1111, 1, SYSDATE, SYSDATE, 1, 0, 0, 'kakao', '킹트', '잘부탁드려요', 'trainer1.png', null, null, null, 0, 0);
+INSERT INTO USERS values (40, 'trainer3@letsports.com', 1111, 1, SYSDATE, SYSDATE, 1, 0, 0, 'naver', '홍트', '잘부탁드려요', 'trainer1.png', null, null, null, 0, 0);
 
 -- 유저 관심 운동 종목 3개
 INSERT INTO USER_INTEREST values (USER_INTEREST_SEQ.NEXTVAL, 10, 1);
 INSERT INTO USER_INTEREST values (USER_INTEREST_SEQ.NEXTVAL, 10, 2);
 INSERT INTO USER_INTEREST values (USER_INTEREST_SEQ.NEXTVAL, 10, 3);
 
--- 트레이너 상세정보 3개
+-- 트레이너 상세정보
 INSERT INTO TRAINER_INFO values (1, 10, 3, '김종석', '트레이너자격증', '신촌센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
 INSERT INTO TRAINER_INFO values (2, 11, 4, '김보현', '트레이너자격증', '홍대센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
 INSERT INTO TRAINER_INFO values (3, 12, 5, '박찬소', '트레이너자격증', '합정센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
@@ -25,20 +35,57 @@ INSERT INTO TRAINER_INFO values (4, 13, 6, '김나현', '트레이너자격증',
 INSERT INTO TRAINER_INFO values (5, 14, 7, '천현정', '트레이너자격증', '고양센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
 INSERT INTO TRAINER_INFO values (6, 15, 8, '이병헌', '트레이너자격증', '영등포센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
 
--- 임시 모임(meeting) 3개 
-INSERT INTO MEETING values (5, 11, 10, 6, 0, SYSDATE, SYSDATE, SYSDATE, SYSDATE, 0, 1, '족구합시다.', '족구하면 얼마나 좋게요~!? 다같이 합시다.', 0, null, 0, null, 0);
-INSERT INTO MEETING values (6, 12, 8, 4, 3, SYSDATE, SYSDATE, SYSDATE, SYSDATE, 0, 1, '볼링합시다.', '볼링하면 얼마나 좋게요~!? 다같이 합시다.', 0, null, 0, null, 0);
-INSERT INTO MEETING values (7, 13, 12, 10, 1, SYSDATE, SYSDATE, SYSDATE, SYSDATE, 0, 1, '축구합시다.', '축구하면 얼마나 좋게요~!? 다같이 합시다.', 0, null, 0, null, 0);
+INSERT INTO trainer_info values (7, 38, 1, '강철종', '트레이너자격증', '신촌센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
+INSERT INTO trainer_info values (8, 39, 7, '김갑수', '트레이너자격증', '영등포센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
+INSERT INTO trainer_info values (9, 40, 10, '홍백현', '트레이너자격증', '강남센터', '안녕하세요. 반갑습니다. 잘부탁드립니다.', SYSDATE);
+
+-- 임시 모임(meeting)
+INSERT INTO meeting VALUES (10, 39, 6, 3, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-09', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (11, 40, 4, 3, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-10', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (12, 38, 4, 4, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-14', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (13, 39, 6, 5, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-08', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (14, 40, 8, 4, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-20', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (15, 38, 8, 6, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-06', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 3, '개인사유취소', 0, NULL, 0);
+INSERT INTO meeting VALUES (16, 39, 8, 6, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-08', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (17, 40, 8, 4, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-17', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
+INSERT INTO meeting VALUES (18, 38, 5, 5, 0, TO_DATE('2021-02-03 18:24:30', 'yyyy-mm-dd hh24:mi:ss'), TO_DATE('2021-02-03', 'yyyy-mm-dd'), TO_DATE('2021-02-05', 'yyyy-mm-dd'), TO_DATE('2021-02-08', 'yyyy-mm-dd'), 0, 0, '테스트', '테스트', 0, NULL, 0, NULL, 0);
 
 -- 임시 모임 참가자 테이블 테스트케이스 
-INSERT INTO MEETING_PARTICIPANTS VALUES (1, 1, 30, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (2, 1, 31, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (3, 2, 30, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (4, 2, 31, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (5, 1, 32, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (6, 2, 32, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (7, 1, 33, SYSDATE, 1, null);
-INSERT INTO MEETING_PARTICIPANTS VALUES (8, 2, 33, SYSDATE, 1, null);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 39, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 30, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 31, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 32, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 33, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 34, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 35, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 36, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 10, 37, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 11, 40, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 11, 30, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 11, 31, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 12, 38, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 12, 32, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 12, 33, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 12, 34, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 13, 39, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 14, 40, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 14, 30, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 14, 31, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 15, 38, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 15, 32, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 16, 39, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 16, 33, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 16, 34, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 16, 35, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 16, 36, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 16, 38, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 17, 40, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 17, 30, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 18, 38, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 18, 31, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 18, 32, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 18, 33, SYSDATE, 1, NULL);
+INSERT INTO meeting_participants VALUES (meeting_participants_seq.nextval, 18, 34, SYSDATE, 1, NULL);
 
 -- 리뷰 테이블 평가 관련 테스트케이스
 INSERT INTO REVIEW values (5, 10, 4.0, 5, '꾀 괜찮았어요', SYSDATE, 0, 11);
