546aeb8b03427ed9e4b4da7c01b5d0c7961e1737
wjdgh823 <wjdgh823@naver.com>
2021-02-18 18:49:48 +0900

    추가

--- Project/src/main/java/com/koreait/project/jungho/command/WishTrainerCommand/WishTrainerInsertCommand.java
+++ Project/src/main/java/com/koreait/project/jungho/command/WishTrainerCommand/WishTrainerInsertCommand.java
@@ -10,22 +10,16 @@ import com.koreait.project.common.CommonMapCommand;
 import com.koreait.project.jungho.dao.WishTrainerDao;
 
 public class WishTrainerInsertCommand implements CommonMapCommand {
-
+	
 	@Override
 	public Map<String, Object> execute(SqlSession sqlSession, Model model) {
-
 		Map<String, Object> map = model.asMap();
 		int scrap_referer_no = (int)map.get("scrap_referer_no");
 		int user_no = (int)map.get("user_no");
-		
 		WishTrainerDao wishTrainerDao = sqlSession.getMapper(WishTrainerDao.class);
-		
 		Map<String, Object> result = new HashMap<String, Object>();
-		
 		int results = wishTrainerDao.WishTrainerInsert(user_no, scrap_referer_no);
-		
 		result.put("result", results);
-		
 		return result;
 	}
 
